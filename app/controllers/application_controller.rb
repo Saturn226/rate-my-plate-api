b83061435cd66916b91c7f7d3391c1702e9938ad
github username <github email address>
2018-01-10 03:18:26 -0500

    added current_user helper to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-    
+   
+private
+    def current_user
+        user = User.first #returning first user for test purposes
+        user
+    end
 end
