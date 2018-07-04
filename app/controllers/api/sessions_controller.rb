538fcf61cfac7c05ed58a429ae2fee1d01cae82b
Saturn226 <serenehopes@gmail.com>
2018-07-04 13:17:58 -0400

    removed password_digest, from rendered json user object

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::SessionsController < ApplicationController
     def login
         user = User.find_by(username: params[:user][:username] ) 
         if user && user.authenticate(params[:user][:password])
-          render json: {token: Auth.create_token({username: params[:user][:username], first_name: params[:user][:first_name], last_name: params[:user][:last_name]}), user: user}
+          render json: {token: Auth.create_token({username: params[:user][:username], first_name: params[:user][:first_name], last_name: params[:user][:last_name]}), user: user.as_json(except: [:password_digest])}
         else
           render json: {errors: "username or password wrong"}, status: 500
         end
